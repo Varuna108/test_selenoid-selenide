60077e88c0214782e03637afc99b26b3db4d2954
Varuna108 <varuna108@yandex.ru>
2020-06-07 18:03:38 +0300

    change setting for start selenoid

--- src/test/java/tests/GoogleSearchTests.java
+++ src/test/java/tests/GoogleSearchTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Description;
@@ -8,12 +9,16 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.GooglePage;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 
 @Tag("google")
@@ -22,10 +27,14 @@ class GoogleSearchTests {
     @BeforeClass
     public static void setUp() throws MalformedURLException {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        final DesiredCapabilities caps = DesiredCapabilities.chrome();
-        caps.setCapability("enableVNC", true);
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://autotest.su:4444/wd/hub"), caps);
-        WebDriverRunner.setWebDriver(driver);
+        Configuration.browser = "chrome";
+        DesiredCapabilities browser = new DesiredCapabilities();
+        browser.setBrowserName("chrome");
+        browser.setVersion("66");
+        browser.setCapability("enableVNC", true);
+        RemoteWebDriver driver = new RemoteWebDriver(URI.create("http://autotest.su:4444/wd/hub").toURL(), browser);
+        setWebDriver(driver);
+        driver.manage().window().setSize(new Dimension(1920, 1080));
     }
 
     @After
