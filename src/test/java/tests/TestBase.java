c5db2fcca88d1ae18b8eee6d1a54fbc4b9af86eb
Varuna108 <varuna108@yandex.ru>
2020-06-07 22:25:31 +0300

    testbase upd

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,17 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.CustomWebDriver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static helpers.AttachmentsHelper.*;
@@ -13,9 +19,13 @@ import static helpers.AttachmentsHelper.*;
 public class TestBase {
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws MalformedURLException {
+        final DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("enableVNC", true);
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://autotest.su:4444/wd/hub"), caps);
+        WebDriverRunner.setWebDriver(driver);
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-       Configuration.browser = CustomWebDriver.class.getName();
+        Configuration.browser = CustomWebDriver.class.getName();
     }
 
 
